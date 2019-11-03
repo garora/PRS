67457a4ce3f861dfde341681e1b708708e4984d3
Marianne Linardos <mlinardos.edu@gmail.com>
2019-11-02 23:56:29 -0400

    little fixes

--- prs-ng/src/app/feature/user/user-login/user-login.component.ts
+++ prs-ng/src/app/feature/user/user-login/user-login.component.ts
@@ -4,52 +4,46 @@ import { UserService } from '../../../service/user.service';
 import { SystemService } from '../../../service/system.service';
 import { Users } from '../../../model/users.class';
 
-@Component( {
+@Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: [ './user-login.component.css' ]
-} )
-
-export class UserLoginComponent implements OnInit
-{
+  styleUrls: ['./user-login.component.css'],
+})
+export class UserLoginComponent implements OnInit {
   message: any;
-  user: Users = new Users( 0, '', '', '', '', '', '', false, false );
+  user: Users = new Users(0, '', '', '', '', '', '', false, false);
   users: Users[];
   // this.systemsvc.clearLogin();
 
-  constructor ( private userSvc: UserService,
+  constructor(
+    private userSvc: UserService,
     private systemSvc: SystemService,
-    private router: Router ) { }
+    private router: Router,
+  ) {}
 
-  ngOnInit ()
-  {
-    this.userSvc.list().subscribe( resp =>
-    {
+  ngOnInit() {
+    this.userSvc.list().subscribe((resp) => {
       this.users = resp;
-    } );
-
+    });
   }
 
-  login ()
-  {
-
-    this.userSvc.login( this.user.username, this.user.password ).subscribe( resp =>
-    {
-      var user = resp as Users;
-      this.systemSvc.data.user.instance = user;
-      this.systemSvc.data.user.loggedIn = true;
-      this.router.navigateByUrl( '/user/list' );
-    }
-      ,
-      err =>
-      {
-        this.message = 'login error - please enter a registered login and password';
-      }
+  login() {
+    this.userSvc.login(this.user.username, this.user.password).subscribe(
+      (resp) => {
+        var user = resp as Users;
+        this.systemSvc.data.user.instance = user;
+        this.systemSvc.data.user.loggedIn = true;
+        this.router.navigateByUrl('/user/list');
+      },
+      (err) => {
+        this.message =
+          'login error - please enter a registered login and password';
+      },
     );
     // this.userSvc.login( this.user.username, this.user.password )
     //   .subscribe( resp =>
     //   {
-    //     this.user = resp as Users;   // taking authentication of user and applying to user and saves                
+    //     this.user = resp as Users;   // taking authentication of user and applying to user and saves
     //     this.systemSvc.setLoggedInUser( this.user );
     //     console.log( 'Logged in as ' + this.user.username )
     //     /*  this.systemSvc.data.user.instance = this.user;
@@ -65,6 +59,6 @@ export class UserLoginComponent implements OnInit
   }
 }
 
- // this.user.userName = 'userName';
-  // this.user.password = 'password';
-  // this.systemSvc.data.setLoggedInUser(this.user);
+// this.user.userName = 'userName';
+// this.user.password = 'password';
+// this.systemSvc.data.setLoggedInUser(this.user);
--- prs-ng/src/app/service/user.service.ts
+++ prs-ng/src/app/service/user.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
-import {HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Users } from '../model/users.class';
 
 @Injectable({
-  providedIn: 'root'
-  })
-  export class UserService {
+  providedIn: 'root',
+})
+export class UserService {
   url = 'https://localhost:44379/api/user';
 
-  constructor( private http: HttpClient
-  ) {}
-  
+  constructor(private http: HttpClient) {}
+
   login(uname: string, pwd: string): Observable<Users> {
-  return this.http.get(this.url + "/" + uname + '/' + pwd) as Observable<Users>;
+    return this.http.get(this.url + '/' + uname + '/' + pwd) as Observable<
+      Users
+    >;
   }
   list(): Observable<Users[]> {
-    return this.http.get(this.url)as Observable<Users[]>;
+    return this.http.get(this.url) as Observable<Users[]>;
   }
   get(id: number): Observable<Users> {
-    return this.http.get(this.url + "/" + id)as Observable<Users>;
+    return this.http.get(this.url + '/' + id) as Observable<Users>;
   }
   create(user: Users): Observable<any> {
     return this.http.post(this.url, user) as Observable<any>;
-  }   //  must provide id as defined in user api controller  
+  } //  must provide id as defined in user api controller
   edit(user: Users): Observable<any> {
-  return this.http.put(this.url + '/' + user.id, user)as Observable<any>;
+    return this.http.put(this.url + '/' + user.id, user) as Observable<any>;
   }
   delete(id: number): Observable<any> {
-  return this.http.delete(this.url + '/' + id);
+    return this.http.delete(this.url + '/' + id);
   }
-
- }
+}
